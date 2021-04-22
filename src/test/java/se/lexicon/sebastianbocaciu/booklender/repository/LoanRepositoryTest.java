3ff8ad0718ef434187f05636837568e9a112c1be
Sebastian Bocaciu <75611075+TheAppleJuice@users.noreply.github.com>
2021-04-22 16:43:14 +0200

    Added LoanRepository + tested functionality

--- src/test/java/se/lexicon/sebastianbocaciu/booklender/repository/LoanRepositoryTest.java
+++ src/test/java/se/lexicon/sebastianbocaciu/booklender/repository/LoanRepositoryTest.java
@@ -1,12 +1,75 @@
 package se.lexicon.sebastianbocaciu.booklender.repository;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import se.lexicon.sebastianbocaciu.booklender.entity.Book;
+import se.lexicon.sebastianbocaciu.booklender.entity.LibraryUser;
+import se.lexicon.sebastianbocaciu.booklender.entity.Loan;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
 class LoanRepositoryTest {
+
+    Book book;
+    LibraryUser user;
+
+    Loan testObject;
+
+    LoanRepository loanRepository;
+
+    @Autowired
+    public void setLoanRepository(LoanRepository loanRepository) {
+        this.loanRepository = loanRepository;
+    }
 
     @BeforeEach
     void setUp() {
+
+        testObject = new Loan();
+        testObject.setLoanTaker(user);
+        testObject.setBook(book);
+        testObject.setTerminated(true);
+
+        loanRepository.save(testObject);
+
     }
+
+    @Test
+    public void test_findById(){
+        List<Loan> loanList = new ArrayList<>();
+        loanRepository.findAll().iterator().forEachRemaining(loanList::add);
+        Long expectedId = loanList.get(0).getLoanId();
+        Optional<Loan> actualId = loanRepository.findById(expectedId);
+
+        assertEquals(user, actualId.get().getLoanTaker());
+    }
+
+    @Test
+    public void test_findAll (){
+        List<Loan> loanList = new ArrayList<>();
+        loanRepository.findAll().iterator().forEachRemaining(loanList::add);
+
+        assertEquals(1, loanList.size());
+    }
+
+    @Test
+    public void test_delete(){
+        List<Loan> loanList = new ArrayList<>();
+        loanRepository.delete(testObject);
+        List<Loan> emptyList = new ArrayList<>();
+        loanRepository.findAll().iterator().forEachRemaining(loanList::add);
+
+        assertEquals(emptyList, loanList);
+    }
+
+
+
+
 }
